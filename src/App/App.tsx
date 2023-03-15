226abdb39674fa4cdd70142bb08b366937c70bdc
lilmonk3y <christiannahuelrivera@gmail.com>
2023-03-15 16:39:16 -0300

    refactorizar TodoContainer para usar render props

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import './App.css';
 import {Title} from "../Title/Title";
 import {TodoSearch} from "../TodoSearch/TodoSearch";
-import {TodoContainer} from "../TodoContainer/TodoContainer";
+import {ErrorList, LoadingList, TodoList, EmptyList, PlaceholderList} from "../TodoContainer/TodoList";
 import {TodoAdd} from "../TodoAdd/TodoAdd";
 import useTodos from "./useTodos";
+import {TodoItem} from "../TodoItem/TodoItem";
+import {visibleTodos} from "../Todo/Todo";
 
 function App() {
   const {
@@ -24,7 +26,31 @@ function App() {
 
       <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
 
-      <TodoContainer todos={todos} loading={loading} searchValue={searchValue} error={error} setTodos={setTodos}/>
+      <TodoList
+          loading={loading}
+          error={error}
+          todos={visibleTodos(todos,searchValue)}
+          totalTodos={todos}
+          onError={() => <ErrorList/>}
+          onLoading={() => <LoadingList/>}
+          onEmpty={() => <EmptyList searchPhrase={searchValue}/>}
+          onFirstTodo={() => <PlaceholderList/>}
+          // render={todo => (
+          //     <TodoItem
+          //         key={todo.text}
+          //         text={todo.text}
+          //         completed={todo.completed}
+          //         setTodos={setTodos}
+          //     />)}
+      >
+          {todo => (
+              <TodoItem
+                  key={todo.text}
+                  text={todo.text}
+                  completed={todo.completed}
+                  setTodos={setTodos}
+              />)}
+      </TodoList>
 
       <TodoAdd setModalVisible={setModalVisible} setTodos={setTodos} modalVisible={modalVisible} />
   </>
---
+++ src/TodoContainer/TodoList.tsx
@@ -0,0 +1,66 @@
+import './TodoContainer.css';
+import React, {ReactElement, ReactNode} from "react";
+import {Todo} from "../Todo/Todo";
+
+function LoadingList() {
+  return <p>Loading ...</p>;
+}
+
+function ErrorList() {
+  return <p>Error al cargar la información de la aplicación</p>;
+}
+
+interface EmptyListType {
+  searchPhrase : string,
+}
+
+function EmptyList({searchPhrase} : EmptyListType) {
+  return <p> No hay resultados para la busqueda {searchPhrase} </p>;
+}
+
+function PlaceholderList() {
+  return <p> Crea tu primer todo </p>;
+}
+
+interface TodoListType {
+  loading :boolean,
+  error :boolean,
+  todos :Todo[],
+  onError : () => ReactNode,
+  onLoading : () => ReactNode,
+  onEmpty : () => ReactNode,
+  onFirstTodo : () => ReactNode,
+  totalTodos : Todo[],
+  render? : (todo : Todo) => ReactNode,
+  children? : (todo : Todo) => ReactNode,
+}
+
+type ListRender = (todo : Todo) => ReactNode;
+function renderFunc(render: ListRender | undefined, children: ListRender | undefined) : ListRender {
+    return render ? (render as ListRender) : (children as ListRender);
+}
+
+function TodoList({
+                    loading,
+                    error,
+                    todos,
+                    onError,
+                    onLoading,
+                    render,
+                    onEmpty,
+                    totalTodos,
+                    children,
+                    onFirstTodo
+                  } : TodoListType) {
+  return (
+    <ul className='todo_container'>
+      {error && onError()}
+      {loading && !error && onLoading()}
+      {!loading && !error && todos.length === 0 && totalTodos.length !== 0 && onEmpty()}
+      {!loading && !error && todos.length === 0 && totalTodos.length === 0 && onFirstTodo()}
+      {!loading && !error && todos.map(renderFunc(render, children))}
+    </ul>
+  );
+}
+
+export {TodoList, ErrorList, LoadingList, EmptyList, PlaceholderList}
