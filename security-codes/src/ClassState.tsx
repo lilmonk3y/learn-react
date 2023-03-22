34c1ed3362158d94e3d050c1d3e2ce168b3b40ba
lilmonk3y <christiannahuelrivera@gmail.com>
2023-03-22 11:30:02 -0300

    Add same flow to the ClassState component as the other components

--- security-codes/src/ClassState.tsx
+++ security-codes/src/ClassState.tsx
@@ -8,6 +8,8 @@ interface ClassStateState {
     error: boolean,
     loading: boolean,
     value: string,
+    confirmed: boolean,
+    deleted: boolean,
 }
 
 const SECURITY_CODE = 'ClassState';
@@ -20,45 +22,69 @@ class ClassState extends React.Component<ClassStateProps, ClassStateState> {
             error: false,
             loading: false,
             value: '',
+            confirmed: false,
+            deleted: false,
         };
     }
 
     componentDidUpdate() {
         if(this.state.loading) {
             setTimeout( () => {
-                this.setState({loading:false});
-
                 if(SECURITY_CODE !== this.state.value) {
-                    this.setState({error: true});
+                    this.setState({error: true, loading: false});
+                } else {
+                    this.setState({confirmed: true, loading: false});
                 }
             }, 1000);
         }
     }
 
     render() {
-        return (
-            <div>
-                <h1>Eliminar {this.props.name}</h1>
-                <p>Por favor escribe el código de seguridad</p>
+        if(!this.state.confirmed && !this.state.deleted) {
+            return (
+                <div>
+                    <h1>Eliminar {this.props.name}</h1>
+                    <p>Por favor escribe el código de seguridad</p>
 
-                {!this.state.loading && this.state.error && <p> Error: El código ingresado es incorrecto </p> }
+                    {!this.state.loading && this.state.error && <p> Error: El código ingresado es incorrecto </p>}
 
-                { this.state.loading && <p> Cargando... </p> }
+                    {this.state.loading && <p> Cargando... </p>}
 
-                <div className='security-code'>
-                    <input
-                        placeholder='Código de seguridad'
-                        value={this.state.value}
-                        onInput={(event: React.ChangeEvent<HTMLInputElement>) => this.setState({value: event.target.value})}
-                    />
-                    <button
-                        onClick={() => this.setState({loading: true, error: false})}
-                    >
-                        Comprobar
-                    </button>
+                    <div className='security-code'>
+                        <input
+                            placeholder='Código de seguridad'
+                            value={this.state.value}
+                            onInput={(event: React.ChangeEvent<HTMLInputElement>) => this.setState({value: event.target.value})}
+                        />
+                        <button
+                            onClick={() => this.setState({loading: true, error: false})}
+                        >
+                            Comprobar
+                        </button>
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        } else if( this.state.confirmed){
+            return (
+                <>
+                    <p>Estás seguro que querés borrarlo?</p>
+                    <button onClick={() => this.setState({deleted: true, confirmed: false})}>
+                        Si, confirmado
+                    </button>
+                    <button onClick={() => this.setState({confirmed:false})}>
+                        No, ahora no
+                    </button>
+                </>);
+        } else {
+            return (
+                <>
+                    <p>¿Querés reiniciar la sección?</p>
+                    <button onClick={() => this.setState({confirmed: false, deleted: false})}>
+                        Si, reiniciemos
+                    </button>
+                </>
+            )
+        }
     }
 }
 
