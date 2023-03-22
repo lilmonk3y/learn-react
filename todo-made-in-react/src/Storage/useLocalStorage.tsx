ad11bab77e070c94bd1ef1037e327ef21a41e924
lilmonk3y <christiannahuelrivera@gmail.com>
2023-03-22 18:29:59 -0300

    Refactor the multiple states in useLocalStorage to use useReducer

--- todo-made-in-react/src/Storage/useLocalStorage.tsx
+++ todo-made-in-react/src/Storage/useLocalStorage.tsx
@@ -3,74 +3,115 @@ import {Todo} from "../Todo/Todo";
 
 const localStorageKey = 'MY_TODOS_V1';
 
-const initialValue : Todo[] = [
-  {text:"comprar lechuga", completed: false},
-  {text:"comprar papa", completed: true},
-  {text:"comprar tomate", completed: false}];
+const initialItems = (key: string) => {
+    const initialValue : Todo[] = [
+        {text:"comprar lechuga", completed: false},
+        {text:"comprar papa", completed: true},
+        {text:"comprar tomate", completed: false}
+    ];
+
+    const localStorageItem = localStorage.getItem(key);
+    let parsedItem: Todo[];
+
+    if (!localStorageItem) {
+        localStorage.setItem(key, JSON.stringify(initialValue));
+        parsedItem = initialValue;
+    } else {
+        parsedItem = JSON.parse(localStorageItem as string) as Todo[];
+    }
+    return parsedItem;
+}
+
+interface UseLocalStorageStateType {
+    error: boolean,
+    loading: boolean,
+    item: Todo[],
+    synchronized: boolean,
+}
+
+enum ActionTypes {
+    SAVE,
+    ERROR,
+    SUCCESS,
+    SYNCHROSNIZE,
+}
 
 function useLocalStorage() {
-  const [error, setError] = React.useState(false);
-  const [loading, setLoading] = React.useState(true);
-  const [item, setItem] = React.useState(initialValue);
-  const [synchronized, setSynchronized] = React.useState(true);
+    const [state, dispatch] = React.useReducer(reducer, {
+        error: false,
+        loading: true,
+        item: initialItems(localStorageKey),
+        synchronized: true,
+    });
 
-  const onSuccess = (parsedItem: Todo[]) => {
-    setError(false);
-    setItem(parsedItem);
-    setSynchronized(true);
-    setLoading(false);
-  }
+    const onSuccess = (parsedItem: Todo[]) => {
+        dispatch({type: ActionTypes.SUCCESS, payload: parsedItem});
+    }
 
-  const onError = () => {
-    setError(true);
-  }
+    const onError = () => {
+        dispatch({type: ActionTypes.ERROR});
+    }
 
-  const onSave = (items: Todo[]) => {
-    setItem(items);
-  }
+    const onSave = (items: Todo[]) => {
+        dispatch({type: ActionTypes.SAVE, payload: items});
+    }
 
-  const onSynchronize = () => {
-    setLoading(true);
-    setSynchronized(false);
-  }
+    const onSynchronize = () => {
+        dispatch({type: ActionTypes.SYNCHROSNIZE});
+    }
 
-  React.useEffect(() => {
-    setTimeout(() => {
-      try {
-        const localStorageItem = localStorage.getItem(localStorageKey);
-        let parsedItem: Todo[];
+    React.useEffect(() => {
+        setTimeout(() => {
+            try {
+                onSuccess(initialItems(localStorageKey));
+            } catch(error) {
+                console.log('failed to load data with error: ' + error);
+                onError();
+            }
+        }, 1000);
+    }, [state.synchronized]);
 
-        if (!localStorageItem) {
-          localStorage.setItem(localStorageKey, JSON.stringify(initialValue));
-          parsedItem = initialValue;
-        } else {
-          parsedItem = JSON.parse(localStorageItem as string) as Todo[];
-        }
+    const saveItem = (items : Todo[]) => {
+        localStorage.setItem(localStorageKey, JSON.stringify(items));
+        onSave(items);
+    }
 
-        onSuccess(parsedItem);
-      } catch(error) {
-        console.log('failed to load data with error: ' + error);
-        onError();
-      }
-    }, 1000);
-  }, [synchronized]);
+    const synchronize = () => onSynchronize();
 
-  const saveItem = (items : Todo[]) => {
-    localStorage.setItem(localStorageKey, JSON.stringify(items));
-    onSave(items);
-  }
-
-  const synchronize = () => {
-    onSynchronize();
-  }
-
-  return {
-    error,
-    loading,
-    item,
-    saveItem,
-    synchronize,
-  };
+    return {
+        error: state.error,
+        loading: state.loading,
+        item: state.item,
+        saveItem,
+        synchronize,
+    };
 }
+
+type ReducerActionType = {type: ActionTypes, payload?: Todo[]};
+
+const reducerObject = (state: UseLocalStorageStateType, action: ReducerActionType) => ({
+    [ActionTypes.SUCCESS]: {
+        ...state,
+        loading: false,
+        synchronized: true,
+        items: (action.payload as Todo[]),
+    },
+    [ActionTypes.ERROR]: {
+        ...state,
+        error: true,
+    },
+    [ActionTypes.SAVE]: {
+        ...state,
+        items: (action.payload as Todo[]),
+    },
+    [ActionTypes.SYNCHROSNIZE]: {
+        ...state,
+        loading: true,
+        synchronized: false,
+    }
+})
+
+const reducer = (state: UseLocalStorageStateType, action: ReducerActionType) : UseLocalStorageStateType =>
+    reducerObject(state, action)[action.type];
 
 export {useLocalStorage, localStorageKey}
